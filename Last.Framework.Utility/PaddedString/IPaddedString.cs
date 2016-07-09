4f9a7549542b7429abde5da2a35f063de1f14c78
DummkopfOfHachtenduden <dummkopfhachtenduden@gmail.com>
2016-07-09 22:28:43 +0200

    Initial

---
+++ Last.Framework.Utility/PaddedString/IPaddedString.cs
@@ -0,0 +1,8 @@
+﻿namespace Replace.Framework.Utility.PaddedString
+{
+    public interface IPaddedString
+    {
+        string Value { get; set; }
+        int Padding { get; }
+    }
+}
---
+++ Last.Server.Download/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Last.Server.Download
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
